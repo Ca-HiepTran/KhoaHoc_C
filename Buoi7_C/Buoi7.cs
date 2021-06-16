4053bbe6e4e83718e1c552258ed75f5518eca833
Ca-HiepTran <84852840+Ca-HiepTran@users.noreply.github.com>
2021-06-16 22:32:26 +0700

    bai tap buoi 7

--- Buoi7_C/Buoi7.cs
+++ Buoi7_C/Buoi7.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Buoi7_C
 {
     class Buoi7
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //VD1();
+            //VD2();
+            TranTheHiep_0968880402 _TenClass = new TranTheHiep_0968880402();
+            _TenClass.Test_class();
+            Console.ReadLine();
+            
         }
+        static void VD1()
+        {
+            //Danh sách đa chiều Array
+            List<string> _lts_str = new List<string>(); //Khai báo danh sách
+            //Khởi tạo các phần tử;
+            string _b1 = "b"; //0
+            string _b2 = "a"; //1
+            string _b3 = "c"; //2
+            //Thêm các phần tử đó vào danh sách
+            _lts_str.Add(_b1);
+            _lts_str.Add(_b2);
+            _lts_str.Add(_b3);
+            //in ra màn hình
+            /*foreach(string _item_str in _lts_str)
+            {
+                Console.WriteLine(_item_str);
+            }
+            Console.ReadLine();*/
+            string[] _mang_str = { _b1, _b2, _b3 }; //Thêm 1 mảng vào trong danh sách
+            _lts_str.AddRange(_mang_str);
+            _lts_str.Sort(); //Sắp xếp theo alphabet
+            for (int i = 0; i < _lts_str.Count; i++)
+            {
+                Console.WriteLine(_lts_str[i]);
+            }
+            Console.ReadLine();
+        }
+        static void VD2()
+        {
+            //Tạo danh sách mảng
+            List<string[]> _lst_mang_str = new List<string[]>();
+            //tạo dòng 1 : tên cột
+            string[] _mang_str_tencot = { "Ten", "Dien Tich","Don Vi" };
+            _lst_mang_str.Add(_mang_str_tencot);
+            //tạo dòng thứ 2: dữ liệu
+            string[] _mang_str_1 = { "Hv1", "35.6", "(cm2)" };
+            _lst_mang_str.Add(_mang_str_1);
+            //tạo dòng thứ 3 : dữ liệu
+            string[] _mang_str_2 = { "Hv2", "24.4", "(cm2)" };
+            _lst_mang_str.Add(_mang_str_2);
+            //in ra màn hình
+            foreach (string[] _item_str in _lst_mang_str)
+            {
+                Console.Write("{0,-10}", _item_str[0]);
+                Console.Write("{0,-15}", _item_str[1]);
+                Console.Write("{0,-10} \n", _item_str[2]);
+            }
+            Console.ReadLine();
+
+        }
+        
     }
 }
---
+++ Buoi7_C/TranTheHiep_0968880402.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Buoi7_C
+{
+    class TranTheHiep_0968880402
+    {
+        public void Test_class()
+        {
+            Bai1();
+            
+            static void Bai1()
+            {
+                
+                string _Hoten_KQ = NhapTen();
+                Console.WriteLine("Day la hoc sinh " + _Hoten_KQ); 
+                double _DiemHoa = NhapDiem("Hoa");
+                double _DiemToan = NhapDiem("Toan");
+                double _DiemLy = NhapDiem("Ly");              
+                double _DTB = Math.Round((_DiemHoa + _DiemLy + _DiemToan) / 3, 2); 
+                string _HocLuc = Danhgiahocluc(_DTB);                             
+                Console.WriteLine("=======TONG KET=======");
+                Console.Write("{0,-20}", "Ho va ten ");
+                Console.Write("{0}", "|");
+                Console.Write("{0,-5}|", "Hoa");
+
+                Console.Write("{0,-5}|", "Toan");
+
+                Console.Write("{0,-5}|", "Ly");
+
+                Console.Write("{0,-5}|", "DTB");
+
+                Console.WriteLine("{0,-10}", "Hoc Luc \n");
+
+                Console.Write("{0,-20}", _Hoten_KQ);
+                Console.Write("{0}", "|");
+                Console.Write("{0,-5}|", _DiemHoa);
+
+                Console.Write("{0,-5}|", _DiemToan);
+
+                Console.Write("{0,-5}|", _DiemLy);
+
+                Console.Write("{0,-5}|", _DTB);
+
+                Console.Write("{0,-10}", _HocLuc);
+                Console.ReadLine();
+            }
+            static string NhapTen() 
+            {
+                Console.Write("Ho ten cua ban la: ");
+                string _Hoten = Console.ReadLine();
+                if (_Hoten != "" && _Hoten != " ") 
+                {
+                    if (!char.IsNumber(_Hoten, 0)) 
+                    {
+                        if (_Hoten.Length > 3) 
+                        {
+                            return _Hoten;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ho ten cua ban qua ngan! ");
+                            NhapTen();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Khong duoc nhap so vao ho ten! ");
+                        NhapTen();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ban chua nhap ho ten cua ban ! ");
+                    NhapTen();
+                }
+                
+                return ""; 
+            }
+            static double NhapDiem(string _TenMonHoc)
+            {
+                double _dbl_Diem = 0.0;
+                Console.Write("Diem mon {0} ban la: ", _TenMonHoc);
+                string _str_Diem = Console.ReadLine();
+                if (_str_Diem != "" && _str_Diem != " ") 
+                {
+                    if (char.IsNumber(_str_Diem, 0)) 
+                    {
+                        _dbl_Diem = double.Parse(_str_Diem);
+                        if (_dbl_Diem >= 0) 
+                        {
+                            if (_dbl_Diem <= 10) 
+                            {
+                                return _dbl_Diem;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Diem khong hop ly! ");
+                                NhapDiem(_TenMonHoc);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ban phai nhap so duong! ");
+                            NhapDiem(_TenMonHoc);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ban phai nhap so! ");
+                        NhapDiem(_TenMonHoc);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ban chua nhap diem! ");
+                    NhapDiem(_TenMonHoc);
+                }
+                return _dbl_Diem;
+            }
+            static string Danhgiahocluc(double _DTB)
+            {
+                string _HocLuc = "";
+                if (_DTB >= 0 && _DTB < 5)
+                {
+                    _HocLuc = "Trung Binh";
+                }
+                else if (_DTB >= 5 && _DTB < 7)
+                {
+                    _HocLuc = "Kha";
+                }
+                else if (_DTB >= 7 && _DTB < 10)
+                {
+                    _HocLuc = "Gioi";
+                }
+                return _HocLuc;
+            }
+        }
+
+    }
+        
+               
+    
+}
